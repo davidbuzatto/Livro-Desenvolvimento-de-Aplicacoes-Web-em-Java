cc97e2e286b5ff4d8e17fde7dc5929ef82e0aedc
David Buzatto <davidbuzatto@gmail.com>
2026-06-14 16:38:53 -0300

    ajustes

--- projetos/capitulo05/CadastroClientes/src/java/cadastroclientes/jdbc/ConnectionFactory.java
+++ projetos/capitulo05/CadastroClientes/src/java/cadastroclientes/jdbc/ConnectionFactory.java
@@ -20,6 +20,14 @@ public class ConnectionFactory {
      */
     public static Connection getConnection() throws SQLException {
 
+        try {
+            // força o registro do driver MariaDB no classloader da aplicação,
+            // necessário para que o Tomcat encontre o driver em WEB-INF/lib
+            Class.forName( "org.mariadb.jdbc.Driver" );
+        } catch ( ClassNotFoundException e ) {
+            throw new SQLException( "Driver MariaDB não encontrado.", e );
+        }
+
         // o método getConnection de DriverManager recebe como parâmetro
         // a URL da base de dados, o usuário usado para conectar na base
         // e a senha deste usuário.
